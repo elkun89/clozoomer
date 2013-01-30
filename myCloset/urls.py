1f07a26afd2a6d2a18e494afd3196e343c9867ca
Bohao Li <bli@wpi.edu>
2013-01-29 20:57:07 -0500

    added ajax functionality, can display profile, apparel, and category

--- myCloset/urls.py
+++ myCloset/urls.py
@@ -3,6 +3,8 @@ from myCloset import views
 
 urlpatterns = patterns('',
     url(r'^apparel/$', views.listApparelByJson, name  = 'listApparelByJson'),
+    url(r'^categories/$', views.listCategoryByJson, name  = 'listCategoryByJson'),
+    url(r'^userProfile/$', views.requestUserProfile, name  = 'requestUserProfile'),
     url(r'^randomTest/$', views.randomTest, name  = 'randomTest'),
 )
 
--- myCloset/views.py
+++ myCloset/views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.models import User, Permission
 from myCloset.forms import *
 from myCloset.models import *
+from myCloset.serializers import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import Context, loader
@@ -10,86 +11,77 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import Http404
 from django.core import serializers
 from django.utils import simplejson
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 import json
 
-def json_response(func):
-    
-    """
-    
-    @json_response
-    
-    A decorator thats takes a view response and turns it into json. If a callback is added
-    through GET or POST the response is JSONP.
-    
-    Example usage:
-    
-    from django_decorators.decorators import json_response
-    @json_response
-    def any_view(request):
-        return {'this will be': 'JSON'}
-    
-    Returns a JSON string.
-    
-    Now, if you need a JSONP response, just add a callback GET or POST variable. :)
-    
-    ---
-    
-    https://github.com/julian-amaya/django-decorators
-    https://gist.github.com/871954
-    https://gist.github.com/1568174
-    
-    """
-    
-    def decorator(request, *args, **kwargs):
-        objects = func(request, *args, **kwargs)
-        callback = request.GET.get('callback', '')
-        if isinstance(objects, HttpResponse):
-            return objects
-        # Even after using `functools.wraps`, I get:
-        # Error: "'dict' object has no attribute 'status_code'".
-#         if objects.status_code != 200:
-#             return objects
-        # It just seems like a good idea to check for a 200 status code... Oh well. :(
-        try:
-            data = simplejson.dumps(objects)
-            if callback:
-                # A jsonp response!
-                data = callback + '(' + data + ');'
-                return HttpResponse(data, 'text/javascript; charset=utf-8')
-        except:
-            data = simplejson.dumps(str(objects))
-        return HttpResponse(data, 'application/json; charset=utf-8')
-    return decorator
-
+#===============================================================================
+# landing function for the index page
+# @param request: the http request sent by user
+# @return: the template with rendered context 
+#===============================================================================
 @login_required
 def landing(request):
-    return render(request, 'index.html')
-
-def viewExample(request, categoryID, exampleID):
     template = loader.get_template('index.html')
     
-    usrCategories = Category.objects.filter(author = request.user);
-    usrApparel = Apparel.objects.filter(owner = request.user)
+    usrCategories = Category.objects.filter(author=request.user);
+    usrApparel = Apparel.objects.filter(owner=request.user);
+    thisUser = request.user;
     
-    context = Context({                                                 # Map the examples in HTML to the examples variable
+    context = Context({  # Map the examples in HTML to the examples variable
                                                                         'usrCategories' : usrCategories,
                                                                         'usrApparel' : usrApparel,
-                                                                        #'dependencies':dependencies,
+                                                                        'thisUser' : thisUser,
+                                                                        # 'dependencies':dependencies,
     })
     return HttpResponse(template.render(context))
 
-@json_response
+#===============================================================================
+# function to give information of the user's apparel
+# @param  request: the ajax GET request 
+# @return: json data of apparel information 
+#===============================================================================
+@login_required
+@api_view(['GET'])
 def listApparelByJson(request):
-    if request.is_ajax:
-        #callback = request.GET.get('callback', '')
-        userApparel = Apparel.objects.filter(owner = request.user)
-        data = serializers.serialize('json', userApparel.all())
-        #data = callback + '(' + data + ');'
-        response = data;
-    else:
-        response = 'fail'
-    return HttpResponse(response, content_type='application/json')
+    if request.method == 'GET':
+        usrApparel = Apparel.objects.filter(owner = request.user);
+        serializer = ApparelSerializer(usrApparel)
+        return Response(serializer.data)
+    
+#===============================================================================
+# function to list all user's categories by json
+# @param request: the ajax request 
+# @return: json data containing category information  
+#===============================================================================
+@login_required
+@api_view(['GET'])
+def listCategoryByJson(request):
+    if request.method == 'GET':
+        usrCategories = Category.objects.filter(author = request.user);
+        serializer = CategorySerializer(usrCategories)
+        return Response(serializer.data)
 
+#===============================================================================
+# function to request the user's information
+# @param  request: the ajax request sent by the user
+# @return : serialized user data in json format  
+#===============================================================================
+@login_required
+@api_view(['GET'])
+def requestUserProfile(request):
+    if request.method == 'GET':
+        users = User.objects.filter(username = request.user.username);
+        serializer = UserSerializer(users)
+        return Response(serializer.data)
+
+#===============================================================================
+# function to do rando jsonp tests
+# @param request: the http request
+# @return: the response containing json data wrapped in jsonp format
+#===============================================================================
+@login_required
 def randomTest(request):
     callback = request.GET.get('callback', '')
     req = {}
@@ -98,21 +90,26 @@ def randomTest(request):
     response = callback + '(' + response + ');'
     return HttpResponse(response, mimetype="application/json")
 
+#===============================================================================
+# function to register users
+# @param request: the http request
+# @return : render form for the user to sign up or upon success redirect to login. 
+#===============================================================================
 def register(request):
-    newUser = User(is_staff = False, is_superuser = False)
+    newUser = User(is_staff=False, is_superuser=False)
     if request.method == 'POST':
-        form = UserForm(request.POST)                                                   #make a user form filled with information from request.post
-        if form.is_valid():                                                             #clean the data and store them in a newUser instance if the form is valid
+        form = UserForm(request.POST)  # make a user form filled with information from request.post
+        if form.is_valid():  # clean the data and store them in a newUser instance if the form is valid
             newUser.username = form.cleaned_data['username']
             newUser.first_name = form.cleaned_data['first_name']
             newUser.last_name = form.cleaned_data['last_name']
             newUser.email = form.cleaned_data['email']
             password = form.cleaned_data['newPassword']
-            newUser.set_password(password)                                              #set the password for the newUser
+            newUser.set_password(password)  # set the password for the newUser
             newUser.save()
             return HttpResponseRedirect('/accounts/login')
     else:
         form = UserForm()
-    return render(request, 'signUp.html',{
+    return render(request, 'signUp.html', {
             'form': form
     })
